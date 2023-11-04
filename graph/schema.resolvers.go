daf342146db8c6c1cbcdd5ff71a7d5091ceb502c
mikaijun <mj.tempe443@gmail.com>
2023-11-04 06:14:34 +0000

    scoreのエラー削除

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,7 +6,6 @@ package graph
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/google/uuid"
@@ -149,13 +148,3 @@ type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type taskResolver struct{ *Resolver }
 type userResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//     it when you're done.
-//   - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *actionResolver) Score(ctx context.Context, obj *model.Action) (int, error) {
-	panic(fmt.Errorf("not implemented: Score - score"))
-}
