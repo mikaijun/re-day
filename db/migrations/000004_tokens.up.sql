f3e24b7e16ceb780b357fffc54f672a6a87f554f
mikaijun <mj.tempe443@gmail.com>
2023-11-18 22:31:08 +0000

    tokensテーブル作成&userを外部キーをつけた

--- db/migrations/000004_tokens.up.sql
+++ db/migrations/000004_tokens.up.sql
@@ -1,12 +1,12 @@
 BEGIN;
-  CREATE TABLE IF NOT EXISTS tasks(
+  CREATE TABLE IF NOT EXISTS tokens(
     id UUID NOT NULL PRIMARY KEY,
-    content TEXT,
+    token VARCHAR(255) NOT NULL,
     user_id UUID NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
-    updated_at TIMESTAMP NOT NULL DEFAULT now(),
     FOREIGN KEY(user_id)
     REFERENCES users(id) 
   );
-  CREATE INDEX on tasks(id);
+ CREATE INDEX on tokens(id);
 COMMIT;
